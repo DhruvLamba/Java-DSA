c0357e4de0d8933fe5127571acdba821271f90b8
Dhruv Lamba <lambadhruv09@gmail.com>
2023-08-16 20:54:39 +0530

    Added pallinddromestring.java

---
+++ pallindromestring.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class pallindromestring {
+  public static boolean pallindrome(String s) {
+    int n = s.length();
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) != s.charAt(n - i - 1)) {
+        return false;
+      }
+    }
+    return true;
+
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String s = sc.nextLine();
+    System.out.println(pallindrome(s));
+  }
+
+}
