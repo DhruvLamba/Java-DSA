da18a1da59cd30814408c23981d5fcbbc91c4bae
Dhruv Lamba <lambadhruv09@gmail.com>
2024-05-30 14:45:09 +0530

    added countrotation.java

---
+++ countrotation.java
@@ -0,0 +1,30 @@
+public class countrotation {
+    public static void main(String[] args) {
+        int[] arr = {6,7,0,1,2,4,5};
+        int count=pivot(arr)+1;
+        System.out.println(count);
+    }
+    public static int pivot(int[] arr){
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(mid<end && arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            else if(arr[mid]<arr[mid-1]){
+                return mid-1;
+            }  
+            else if(arr[start]<=arr[mid]){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+            
+        }
+        return -1;
+    }
+}
+
+
--- peak.java
+++ peak.java
@@ -1,8 +1,43 @@
 class Solution {   
     public static void main(String[] args) {
         int[] arr={0,1,2,3,5,4,3,2,1};
+        int target=3;
         int peek=findPeakElement(arr);
-        System.out.println(peek);
+        int end=arr.length;
+        int left=orderagniostic(arr,target,0,peek,true);
+        if(left!=-1){
+            System.out.println(left);
+        }
+        else{
+            int right=orderagniostic(arr,target,peek+1,end,false);
+            System.out.println(right);
+        }
+    }
+
+    public static int orderagniostic(int[] arr, int target,int start, int end, boolean isAsc){
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            if(isAsc){
+                if(target>arr[mid]){
+                    start=mid+1;
+                }
+                else{
+                    end=mid-1;
+                }
+            }            
+            else{
+                if(target>arr[mid]){
+                    end=mid-1;
+                }
+                else{
+                    start=mid+1;
+                }
+            }
+        }
+        return -1;
     }
  
 
